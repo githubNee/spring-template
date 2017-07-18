dc5446db7ad160b37861a3f01a6cb7fd532f0ed0
Nee <nee_11235@qq.com>
2017-07-18 22:31:43 +0800

    file upload

---
+++ src/main/java/com/example/demo/Controller/FileController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/upload")
+public class FileController {
+    @RequestMapping("/")
+    String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            byte[] bytes = file.getBytes();
+            String content = new String(bytes);
+
+            System.out.println(content);
+            return "yes";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "fuck";
+    }
+}
